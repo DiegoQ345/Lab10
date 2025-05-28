e9b3792bd570e6949a586f2cb7f84ea6cfa028e4
Diego Zahid Quispe Quispe <diego.quispe.qui@tecsup.edu.pe>
2025-05-27 20:53:39 -0500

    Cambio de directorio

--- app/src/main/java/com/quispe/lab10/MainActivity.kt
+++ app/src/main/java/com/quispe/lab10/MainActivity.kt
@@ -18,30 +18,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Lab10Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
+
+
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Lab10Theme {
-        Greeting("Android")
-    }
-}
